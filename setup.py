ea0d8680fa422377b98a8af21b3f904b7ba72b76
Louis Cialdella <lmc2179@github.com>
2015-05-01 21:24:49 -0400

    Added setup requirements for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'chevrons',
+  packages = ['chevrons'],
+  version = '0.1',
+  description = 'Rapidly build pipelines for out-of-core data processing using higher order functions.',
+  author = 'Louis Cialdella',
+  author_email = 'louiscialdella@gmail.com',
+  url = 'https://github.com/lmc2179/chevrons',
+  download_url = 'https://github.com/lmc2179/chevrons/tarball/0.1',
+  keywords = ['parallel', 'functional', 'map', 'reduce', 'fold', 'pipeline', 'stream'],
+  classifiers = [],
+)
