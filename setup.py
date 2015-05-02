7b6fc18b09f28854f09a5b671e675c09e85a06d3
Louis Cialdella <lmc2179@github.com>
2015-05-01 21:51:28 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'chevrons',
   packages = ['chevrons'],
-  version = '0.1',
+  version = '0.1.1',
   description = 'Rapidly build pipelines for out-of-core data processing using higher order functions.',
   author = 'Louis Cialdella',
   author_email = 'louiscialdella@gmail.com',
   url = 'https://github.com/lmc2179/chevrons',
-  download_url = 'https://github.com/lmc2179/chevrons/tarball/0.1',
+  download_url = 'https://github.com/lmc2179/chevrons/tarball/0.1.1',
   keywords = ['parallel', 'functional', 'map', 'reduce', 'fold', 'pipeline', 'stream'],
   classifiers = [],
 )
