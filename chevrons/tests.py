01660007faa2e59ced5ef50d3c085c2e272fef5e
Louis Cialdella <lmc2179@github.com>
2015-05-03 23:02:08 -0400

    separated performance tests from functional tests

--- chevrons/tests.py
+++ chevrons/tests.py
@@ -1,8 +1,6 @@
 import unittest
-from pipeline import Map, Fold, Processor, MapParallel, BatchProcessor
-import datetime
+from pipeline import Map, Fold, Processor, MapParallel, SerialBatchProcessor#, ParallelBatchProcessor
 import numpy as np
-import functools
 
 class Square(Processor):
     def run(self, input_data):
@@ -17,7 +15,7 @@ class SwapInputs(Processor):
         a,b = input_data
         return b,a
 
-class AddOneBatch(BatchProcessor):
+class AddOneSerialBatch(SerialBatchProcessor):
     def _process_batch(self, input_batch):
         return [add_one(i) for i in input_batch]
 
@@ -27,13 +25,12 @@ def square(x):
 def add_one(x):
     return x+1
 
+def add_one_batch(batch):
+    return [x+1 for x in batch]
+
 def infinite_generator():
     while True:
         yield 1
-
-def time_consuming_function(x):
-    "A costly function which takes a float and returns the same float. Used for testing efficiency of parallelism."
-    return float(str(float(str(float(str(float(str(x))))))))
 
 class SyntaxTest(unittest.TestCase):
     def test_pipe(self):
@@ -49,6 +46,7 @@ class SyntaxTest(unittest.TestCase):
     def test_multiple_input(self):
         assert ((1,2) | SwapInputs()) == (2,1)
 
+class FunctionTest(unittest.TestCase):
     def test_map(self):
         assert list([1,2] | Map(square) >> Map(square)  >> Map(add_one)) == [2,17]
 
@@ -60,29 +58,27 @@ class SyntaxTest(unittest.TestCase):
         output_stream = data | Map(square) >> MapParallel(square)
         assert next(output_stream) == 1
 
-    def test_map_parallel_speed(self):
-        test_data_size = 100000
-        data1,data2 = range(test_data_size),range(test_data_size)
-        begin = datetime.datetime.now()
-        output = data1 | Map(time_consuming_function)
-        list(output)
-        single_thread_time = datetime.datetime.now() - begin
-        begin = datetime.datetime.now()
-        output = data1 | MapParallel(time_consuming_function, batch_size=5000)
-        list(output)
-        multi_thread_time = datetime.datetime.now() - begin
-        assert multi_thread_time < single_thread_time
-
     def test_batch_process(self):
         import itertools
         data = itertools.islice(infinite_generator(),0,1000)
-        output = data | AddOneBatch(100) >> AddOneBatch(100)
+        output = data | AddOneSerialBatch(100) >> AddOneSerialBatch(100)
         assert len(list(output)) == 1000
 
     def test_batch_process_infinite(self):
         data = infinite_generator()
-        output = data | AddOneBatch(100) >> AddOneBatch(100)
+        output = data | AddOneSerialBatch(100) >> AddOneSerialBatch(100)
         assert next(output) == 3
+
+    # def test_batch_process_parallel(self):
+    #     import itertools
+    #     data = itertools.islice(infinite_generator(),0,1000)
+    #     output = data | ParallelBatchProcessor(add_one_batch) >> ParallelBatchProcessor(add_one_batch)
+    #     assert len(list(output)) == 1000
+    #
+    # def test_batch_process_parallel_infinite(self):
+    #     data = infinite_generator()
+    #     output = data | ParallelBatchProcessor(add_one_batch) >> ParallelBatchProcessor(add_one_batch)
+    #     assert next(output) == 3
 
 def parse_row( element):
         return np.array([int(element['COL_1']), int(element['COL_2'])])
