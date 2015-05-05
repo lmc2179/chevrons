7b7b70361242f96194bb55b0ca7a30e84ac70da7
Louis Cialdella <lmc2179@github.com>
2015-05-04 21:54:10 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'chevrons',
   packages = ['chevrons'],
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'Rapidly build pipelines for out-of-core data processing using higher order functions.',
   author = 'Louis Cialdella',
   author_email = 'louiscialdella@gmail.com',
