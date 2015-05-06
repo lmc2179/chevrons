c1b7b4df508c11f1874d42711ddd61e264001bce
Louis Cialdella <lmc2179@github.com>
2015-05-05 21:38:00 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'chevrons',
   packages = ['chevrons'],
   py_modules = ['chevrons.pipeline_base','chevrons.pipeline_hof', 'chevrons.pipeline_extra'],
-  version = '0.1.2.2',
+  version = '0.1.2.3',
   description = 'Rapidly build pipelines for out-of-core data processing using higher order functions.',
   author = 'Louis Cialdella',
   author_email = 'louiscialdella@gmail.com',
