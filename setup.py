956db879fd0fda89199839238631eaacdd8befc0
Louis Cialdella <lmc2179@github.com>
2015-05-07 22:53:14 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   description = 'Rapidly build pipelines for out-of-core data processing using higher order functions.',
   author = 'Louis Cialdella',
   author_email = 'louiscialdella@gmail.com',
+  classifiers = ['Programming Language :: Python :: 3 :: Only'],
   url = 'https://github.com/lmc2179/chevrons',
-  keywords = ['parallel', 'functional', 'map', 'reduce', 'fold', 'pipeline', 'stream'],
-  classifiers = [],
+  keywords = ['parallel', 'functional', 'map', 'reduce', 'fold', 'pipeline', 'stream']
 )
